4ca90b6938228c37948ab11a34db75277104ccbd
gazservissana.zp@gmail.com <gazservissana.zp@gmail.com>
2024-02-01 20:51:18 +0200

    Created circles controller

---
+++ Ef_RotatingCircle/Controllers/Circle/CircleViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CircleViewController.swift
+//  Ef_RotatingCircle
+//
+//  Created by Aleksandr on 01.02.2024.
+//
+
+import UIKit
+
+class CircleViewController: UIViewController {
+
+    @IBOutlet private weak var containerView: UIView!
+    @IBOutlet private weak var buttonsStack: UIStackView!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var minusButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    @IBAction private func didTabPlusButton(_ sender: UIButton) {
+    }
+    
+    @IBAction private func didTapMinusButton(_ sender: UIButton) {
+    }
+}
